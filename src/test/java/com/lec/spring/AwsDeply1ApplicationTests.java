69889b49bcc6a19bf9e3720c75ba322ff242a022
seungbin <imsen456@gmail.com>
2023-12-13 09:38:17 +0900

    Initial commit

---
+++ src/test/java/com/lec/spring/AwsDeply1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeply1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
